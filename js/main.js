fabc7357b0d21847427e294649db201d92e9d618
Nicolas <nicolasjavier768@hotmail.com>
2021-03-29 10:53:49 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*const url_ = "https://apipetshop.herokuapp.com/api/articulos"
+let padre = document.getElementById("padre")*/
+/**
+ * propiedades de los objetos
+ * 
+ * descripcion
+ * imagen
+ * nombre
+ * precio
+ * stock
+ * tipo
+ * id
+ * 
+ * myFunction(nData.nombre , padre , "stock" , "precio")
+ */
+
+/*fetch(url_)
+.then(data => data.json())
+.then(nData => myFunction(nData , padre , "id" , "precio") )
+.catch(error => alert("error"))
+
+
+function myFunction(datos , padre , prop1 , prop2){
+    datos.array.forEach(element => {
+        
+    });(element => {
+        padre.innerHTML += `
+        <h2>${"name:" +" "+ element[prop1]}</h2>
+        <h2>${"email:"+" "+ element[prop2]}</h2>
+        <hr>  
+        `
+    })
+}*/
