74859c1e631dbddd3e3dc839a7417c219e517766
Nicolas <nicolasjavier768@hotmail.com>
2021-03-30 02:12:17 -0300

    30

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-/*const url_ = "https://apipetshop.herokuapp.com/api/articulos"
-let padre = document.getElementById("padre")*/
+
 /**
  * propiedades de los objetos
  * 
@@ -8,26 +7,45 @@ let padre = document.getElementById("padre")*/
  * nombre
  * precio
  * stock
- * tipo
+ * tipo juguete medicamento
  * id
  * 
  * myFunction(nData.nombre , padre , "stock" , "precio")
  */
 
-/*fetch(url_)
+
+ const url_ = "https://apipetshop.herokuapp.com/api/articulos"
+
+if(document.getElementById("contenedor-juguetes")){
+    let padre  = document.getElementById("contenedor-juguetes")
+    fetch(url_)
 .then(data => data.json())
-.then(nData => myFunction(nData , padre , "id" , "precio") )
-.catch(error => alert("error"))
+.then(nData => juguetesFuntion(nData.response, padre, "nombre" , "precio"))
+.catch(error => console.log(error))
+}
+else if(document.getElementById("contenedor-farmacia")){
+    let padre  = document.getElementById("contenedor-juguetes")
+    fetch(url_)
+.then(data => data.json())
+.then(nData => juguetesFuntion(nData.response, padre, "nombre" , "precio"))
+.catch(error => console.log(error))
+}
 
-
-function myFunction(datos , padre , prop1 , prop2){
-    datos.array.forEach(element => {
-        
-    });(element => {
-        padre.innerHTML += `
-        <h2>${"name:" +" "+ element[prop1]}</h2>
-        <h2>${"email:"+" "+ element[prop2]}</h2>
-        <hr>  
-        `
+function juguetesFuntion(datos , padre_ , prop1 , prop2 , tipo){
+    datos.forEach((element,index) => {
+        if(element.tipo == "Juguete"){
+            padre_.innerHTML += `
+            <div class="row tarjetas">
+                <div class="col-sm-10 ps-5 py-3">
+                    <a href="${element.imagen}" data-lightbox="roadtrip">
+                        <img class="icon" src="${element.imagen}" alt="logo">
+                    </a> 
+                     nombre: ${element[prop1]}
+                     precio: $${element[prop2]}
+                     tipo: ${element.tipo}
+                </div>
+            </div>
+            `
+        }
     })
-}*/
+}
--- js/particulas.js
+++ js/particulas.js
@@ -1,110 +1,112 @@
-particlesJS({
-    "particles": {
-      "number": {
-        "value": 104,
-        "density": {
-          "enable": true,
-          "value_area": 473.51100518494565
-        }
-      },
-      "color": {
-        "value": "#dcc6c6"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 7.891850086415761,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 500,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 2
-      },
-      "move": {
+particlesJS(
+  {
+  "particles": {
+    "number": {
+      "value": 60,
+      "density": {
         "enable": true,
-        "speed": 6,
-        "direction": "bottom",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+        "value_area": 900
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "bubble"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "resize": true
+    "color": {
+      "value": "#de0fde"
+    },
+    "shape": {
+      "type": "polygon",
+      "stroke": {
+        "width": 0,
+        "color": "#000"
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 0.5
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 4,
-          "duration": 0.3,
-          "opacity": 1,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
-    "retina_detect": true
-  })
+    "opacity": {
+      "value": 0.23234759199762825,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 0.48691418137553294,
+        "opacity_min": 0.048691418137553295,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 112.16780303333778,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 10,
+        "size_min": 40,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 200,
+      "color": "#ffffff",
+      "opacity": 1,
+      "width": 2
+    },
+    "move": {
+      "enable": true,
+      "speed": 8.011985930952699,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 353.01278149726136,
+        "size": 40.57618178129441,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+)
